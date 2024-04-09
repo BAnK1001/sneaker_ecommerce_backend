8ed9f6cec9e7489c0716672d6b9bc2ae1d21abab
CaoHuynhBaoAn1001 <caoan0012345@gmail.com>
2024-04-10 04:59:39 +0700

    add vendor api

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -1,13 +1,33 @@
 const Vendor = require("../models/Vendor");
 
 module.exports = {
+  //api/addVendor
   addVendor: async (req, res) => {
-    const newVendor = new Vendor(req.body);
+    const { title, time, imageUrl, owner, code, logoUrl, coords } = req.body;
+    if (
+      !title ||
+      !time ||
+      !imageUrl ||
+      !owner ||
+      !code ||
+      !logoUrl ||
+      !coords.latitude ||
+      !coords.longitude ||
+      !coords.address ||
+      !coords.title
+    ) {
+      return res
+        .status(400)
+        .json({ status: false, message: "You have a missing field" });
+    }
     try {
-      const savedVendor = await newVendor.save();
-      res.status(200).json(savedVendor);
-    } catch (err) {
-      res.status(500).json(err);
+      const newVendor = new Vendor(req.body);
+      await newVendor.save();
+      res
+        .status(201)
+        .json({ status: true, message: "Vendor created successfully" });
+    } catch (error) {
+      res.status(500).json({ status: false, message: error.message });
     }
   },
   getVendorbyId: async (req, res) => {},
