932caadab719d2e5f67a6514cc72ab3e135cee45
CaoHuynhBaoAn1001 <caoan0012345@gmail.com>
2024-04-11 17:17:18 +0700

    Refactor the code

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
-const express = require("express"),
-  app = express(),
-  dotenv = require("dotenv"),
-  mongoose = require("mongoose"),
-  categoryRoutes = require("./routes/category"),
-  vendorRoutes = require("./routes/vendor"),
-  shoeRoutes = require("./routes/shoe"),
-  ratingRoutes = require("./routes/rating");
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+// Import routes
+const categoryRoutes = require("./routes/category");
+const vendorRoutes = require("./routes/vendor");
+const shoeRoutes = require("./routes/shoe");
+const ratingRoutes = require("./routes/rating");
+
+// Import utility functions
+const generateOTP = require("./utils/otp_generator");
+const sendMail = require("./utils/smtp_function");
 
 dotenv.config();
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB connection successful"))
   .catch((err) => console.log(err));
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Route middleware
 app.use("/api/category", categoryRoutes);
 app.use("/api/vendor", vendorRoutes);
 app.use("/api/shoe", shoeRoutes);
 app.use("/api/rating", ratingRoutes);
 
-app.listen(process.env.PORT || 6013, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`)
-);
+// Define the port number
+const PORT = process.env.PORT || 6013;
+
+// Start the server
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
